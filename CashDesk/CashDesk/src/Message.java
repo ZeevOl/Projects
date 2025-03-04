bb96e643db71f3baf765504135b80aeb85e43cfc
ZeevOl <129842524+ZeevOl@users.noreply.github.com>
2025-03-04 17:08:33 +0200

    Delete CashDesk directory
    deleted

--- CashDesk/CashDesk/src/Message.java
+++
@@ -1,8 +0,0 @@
-public  class Message {
-    public static void showMessage(String s){
-        System.out.println(s);
-    }
-    public static void showMessage(CashDesk myCashDesk){
-        System.out.println(myCashDesk);
-    }
-}
