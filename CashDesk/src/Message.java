609bf9d10180f038d9324ac27731149cb9e1763f
ZeevOl <129842524+ZeevOl@users.noreply.github.com>
2025-03-04 17:10:07 +0200

    Add files via upload
    A simple cash desk operating program.
    Author: Zeev Olan
    Date; 4.3.25

---
+++ CashDesk/src/Message.java
@@ -0,0 +1,8 @@
+public  class Message {
+    public static void showMessage(String s){
+        System.out.println(s);
+    }
+    public static void showMessage(CashDesk myCashDesk){
+        System.out.println(myCashDesk);
+    }
+}
